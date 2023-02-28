2965266b23787e0fe841b7e9105a045a40cacdec
Victor <victor-likes-coding@tempestas.io>
2023-02-28 16:07:17 -0500

    finish displaytests

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Display from './../Display';
 
-test('renders without errors with no props', async () => { });
+test('renders without errors with no props', async () => {
+	// renders with no errors
+	render(<Display />);
+});
 
-test('renders Show component when the button is clicked ', () => { });
+test('renders Show component when the button is clicked ', async () => {
+	render(<Display />);
+	const button = screen.getByRole('button');
+	fireEvent.click(button);
+	await waitFor(() => {
+		expect(screen.getByTestId('show-container')).toBeVisible();
+	});
+});
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test('renders show season options matching your data when the button is clicked', async () => {
+	render(<Display />);
+	const button = screen.getByRole('button');
+	fireEvent.click(button);
+
+	await waitFor(() => {
+		const select = screen.getByRole('combobox');
+		select.change(select, { target: { value: '0' } });
+		expect(screen.getByText('Chapter One: The Vanishing of Will Byers'));
+	});
+});
